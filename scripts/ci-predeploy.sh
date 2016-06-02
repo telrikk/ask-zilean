0e6abf30c58b0683a4f4e8377cb5282e5bf0f93d
Marc Gunn <telrikk@gmail.com>
2016-06-02 00:00:28 -0400

    uglify

--- scripts/ci-predeploy.sh
+++ scripts/ci-predeploy.sh
@@ -2,12 +2,13 @@
 
 mkdir -p /tmp/deploy/src/github.com/telrikk/ask-zilean/
 cd ${TRAVIS_BUILD_DIR}
-rm -rf node_modules
 rm -rf dist
 npm run webpack-production
+npm run uglify
+rm -rf node_modules
 cp -R ./ /tmp/deploy/src/github.com/telrikk/ask-zilean/
 cd /tmp/deploy/
 cp /tmp/deploy/src/github.com/telrikk/ask-zilean/Procfile ./
 cp /tmp/deploy/src/github.com/telrikk/ask-zilean/Buildfile ./
-zip ${TRAVIS_BUILD_DIR}/deploy.zip -x'src/github.com/telrikk/ask-zilean/node_modules/*' -r ./
+zip -q ${TRAVIS_BUILD_DIR}/deploy.zip -x'src/github.com/telrikk/ask-zilean/node_modules/*' -r ./
 cd ${TRAVIS_BUILD_DIR}
