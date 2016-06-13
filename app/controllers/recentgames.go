be67b63ba6aa2c82df21705a5f31ad4b8af7aeda
Marc Gunn <telrikk@gmail.com>
2016-06-12 17:48:02 -0700

    do not need full game for recent games page

--- app/controllers/recentgames.go
+++ app/controllers/recentgames.go
@@ -7,47 +7,32 @@ import (
 	model "github.com/telrikk/ask-zilean/app/recentgames"
 	"github.com/telrikk/ask-zilean/app/util"
 	"github.com/telrikk/lol-go-api/game"
-	"github.com/telrikk/lol-go-api/match"
 )
 
-var queueDescriptions = map[string]string{
-	"ARAM_5x5":                        "(ARAM)",
-	"BILGEWATER_5x5":                  "(Bilgewater)",
-	"BILGEWATER_ARAM_5x5":             "(ARAM)",
-	"ASCENSION_5x5":                   "(Ascension)",
-	"NORMAL_5x5_BLIND":                "(Blind)",
-	"BOT_5x5":                         "(Bots)",
-	"BOT_5x5_BEGINNER":                "(Bots)",
-	"BOT_5x5_INTERMEDIATE":            "(Bots)",
-	"BOT_5x5_INTRO":                   "(Bots)",
-	"BOT_TT_3x3":                      "(Bots)",
-	"BOT_ODIN_5x5":                    "(Bots)",
-	"BOT_URF_5x5":                     "(Bots)",
-	"COUNTER_PICK":                    "(Counter Pick)",
-	"CUSTOM":                          "(Custom)",
-	"NORMAL_5x5_DRAFT":                "(Draft)",
-	"FIRSTBLOOD_1x1":                  "(First Blood)",
-	"FIRSTBLOOD_2x2":                  "(First Blood)",
-	"HEXAKILL":                        "(Hexakill)",
-	"KING_PORO_5x5":                   "(King Poro)",
-	"SR_6x6":                          "(Hexakill)",
-	"NIGHTMARE_BOT_5x5_RANK1":         "(Bots)",
-	"NIGHTMARE_BOT_5x5_RANK5":         "(Bots)",
-	"NIGHTMARE_BOT_5x5_RANK2":         "(Bots)",
-	"ODIN_5x5_BLIND":                  "(Dominion)",
-	"ODIN_5x5_DRAFT":                  "(Odin)",
-	"ONEFORALL_MIRRORMODE_5x5":        "(One For All)",
-	"ONEFORALL_5x5":                   "(One For All)",
-	"RANKED_PREMADE_3x3":              "(Team Ranked)",
-	"RANKED_PREMADE_5x5":              "(Team Ranked)",
-	"RANKED_SOLO_5x5":                 "(Ranked)",
-	"RANKED_TEAM_5x5":                 "(Team Ranked)",
-	"RANKED_TEAM_3x3":                 "(Team Ranked)",
-	"NORMAL_3x3":                      "(Normal)",
-	"GROUP_FINDER_5x5":                "(Team Builder)",
-	"TEAM_BUILDER_DRAFT_UNRANKED_5x5": "(Normal)",
-	"TEAM_BUILDER_DRAFT_RANKED_5x5":   "(Ranked)",
-	"URF_5x5":                         "(URF)",
+var gameSubTypeMap = map[string]string{
+	"BOT":                "(Bots)",
+	"NORMAL":             "(Normal)",
+	"ODIN_UNRANKED":      "(Dominion)",
+	"RANKED_PREMADE_3x3": "(Team Ranked)",
+	"RANKED_PREMADE_5x5": "(Team Ranked)",
+	"NORMAL_3x3":         "(Twisted Treeline)",
+	"BOT_3x3":            "(Bots)",
+	"ARAM_UNRANKED_5x5":  "(ARAM)",
+	"URF":                "(URF)",
+	"URF_BOT":            "(Bots)",
+	"ASCENSION":          "(Ascension)",
+	"NIGHTMARE_BOT":      "(Bots)",
+	"COUNTER_PICK":       "(Counter Pick)",
+	"KING_PORO":          "(King Poro)",
+	"BILGEWATER":         "(Bilgewater)",
+	"HEXAKILL":           "(Hexakill)",
+	"SR_6x6":             "(Hexakill)",
+	"FIRSTBLOOD_1x1":     "(First Blood)",
+	"FIRSTBLOOD_2x2":     "(First Blood)",
+	"ONEFORALL_5x5":      "(One For All)",
+	"RANKED_SOLO_5x5":    "(Ranked)",
+	"RANKED_TEAM_5x5":    "(Team Ranked)",
+	"RANKED_TEAM_3x3":    "(Team Ranked)",
 }
 
 var mapNames = map[string]string{
@@ -68,13 +53,9 @@ func (c App) RecentGames(name string) revel.Result {
 	if err != nil {
 		return util.HandleError(*c.Controller, err)
 	}
-	fullGameData, err := model.GetFullGameData(recentGames)
-	if err != nil {
-		return util.HandleError(*c.Controller, err)
-	}
 	var games []model.RecentGame
 	for _, recentGame := range recentGames {
-		translatedGame := translateRecentGame(recentGame, fullGameData, summonerID, name)
+		translatedGame := translateRecentGame(recentGame, summonerID, name)
 		if translatedGame.ID != 0 {
 			games = append(games, translatedGame)
 		}
@@ -84,18 +65,8 @@ func (c App) RecentGames(name string) revel.Result {
 	return c.RenderJson(response)
 }
 
-func translateRecentGame(game game.Game, gameData map[int]match.Detail, summonerID int, summonerName string) model.RecentGame {
+func translateRecentGame(game game.Game, summonerID int, summonerName string) model.RecentGame {
 	translatedGame := new(model.RecentGame)
-	fullGame := gameData[game.GameID]
-	nameData := make(map[int]string)
-	for _, participant := range fullGame.ParticipantIdentities {
-		// HACK: bad data on Riot's side, will need to use summoner API for non-ranked games
-		nameData[participant.ParticipantID] = participant.Player.SummonerName
-	}
-	for _, player := range fullGame.Participants {
-		translatedPlayer := translatePlayer(player, nameData)
-		translatedGame.Players = append(translatedGame.Players, translatedPlayer)
-	}
 	translatedGame.Summoner = translateSummoner(game, summonerName)
 	mapImageName := util.MapData().Data[strconv.Itoa(game.MapID)].Image.Full
 	translatedGame.MapImageURL = util.MapImagePrefix() + "map/" + mapImageName
@@ -111,7 +82,7 @@ func translateRecentGame(game game.Game, gameData map[int]match.Detail, summoner
 		readableMapName = "Unknown Map"
 	}
 	translatedGame.MapName = readableMapName
-	description, ok := queueDescriptions[fullGame.QueueType]
+	description, ok := gameSubTypeMap[game.SubType]
 	if !ok {
 		description = "(Other)"
 	}
@@ -141,28 +112,6 @@ func translateSummoner(game game.Game, summonerName string) model.Player {
 	translatedPlayer.IsWinner = game.Stats.Win
 	return *translatedPlayer
 
-}
-
-func translatePlayer(player match.Participant, nameData map[int]string) model.Player {
-	translatedPlayer := new(model.Player)
-	translatedPlayer.SummonerName = nameData[player.ParticipantID]
-	translatedPlayer.Kills = player.Stats.Kills
-	translatedPlayer.Assists = player.Stats.Assists
-	translatedPlayer.Deaths = player.Stats.Deaths
-	championImageName := util.ChampionData().Data[strconv.Itoa(player.ChampionID)].Image.Full
-	translatedPlayer.ChampionImageURL = util.ChampionImagePrefix() + "champion/" + championImageName
-	translatedPlayer.ChampionName = util.ChampionData().Data[strconv.Itoa(player.ChampionID)].Name
-	translatedPlayer.Items = []model.Item{}
-	for _, ItemID := range []int{player.Stats.Item0, player.Stats.Item1, player.Stats.Item2,
-		player.Stats.Item3, player.Stats.Item4, player.Stats.Item5, player.Stats.Item6} {
-		if ItemID > 0 {
-			translatedPlayer.Items = append(translatedPlayer.Items, translateItem(ItemID))
-		}
-	}
-	translatedPlayer.CreepScore = player.Stats.MinionsKilled + player.Stats.NeutralMinionsKilled
-	translatedPlayer.Gold = player.Stats.GoldEarned
-	translatedPlayer.IsWinner = player.Stats.Winner
-	return *translatedPlayer
 }
 
 func translateItem(itemID int) model.Item {
